84acc84f53b3abf2a5bb64d15d94021c509dfd64
gkirion <gkirion@gmail.com>
2020-09-13 19:11:38 +0300

    added unsubscribe method

--- src/main/java/pubsub/broker/Brokerable.java
+++ src/main/java/pubsub/broker/Brokerable.java
@@ -5,5 +5,6 @@ public interface Brokerable {
     public void publish(Message message);
     public void publish(String topic, String text);
     public boolean subscribe(String topic, Receivable receivable);
+    public void unsubscribe(String topic, Receivable receivable);
 
 }
